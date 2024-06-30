d87f9ae16cf00a62f506397aad5a7c54545f4c07
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-06-30 18:47:11 +0700

    Added user model

---
+++ app/db/models.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+
+# Base class for models
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[intpk]
+    user_login: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
+    user_name: Mapped[str] = mapped_column(String(40), nullable=False)
+    user_surname: Mapped[str] = mapped_column(String(40), nullable=False)
+    user_email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    hashed_password: Mapped[bytes] = mapped_column(nullable=False)
