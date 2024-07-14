8bd2000df73fa646ff93384e6b119906e67bce74
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-07-14 18:16:56 +0700

    Added sending a message during registration

--- app/auth/manager.py
+++ app/auth/manager.py
@@ -1,7 +1,9 @@
 import os
+import smtplib
 from dotenv import load_dotenv
 from typing import Optional
 
+from email.message import EmailMessage
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin
 from db.models import User
@@ -12,6 +14,20 @@ load_dotenv()
 
 SECRET = os.getenv('SECRET_AUTH_KEY')
 
+login = os.getenv("EMAIL_LOGIN")
+password = os.getenv('EMAIL_PASSWORD') 
+
+def send_message(user_email: str):
+    email = EmailMessage()
+    email['Subject'] = 'Thanks for registration'
+    email['From'] = login
+    email['To'] = 'yura.voskanyan.2003@mail.ru'
+    email.set_content('Test message')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(login, password)
+        server.send_message(email)
+
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = SECRET
@@ -19,7 +35,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
-
+        send_message(user.email)
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional[Request] = None
     ):
