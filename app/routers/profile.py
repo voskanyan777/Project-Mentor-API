c05aeb4baaa5625c6ee8b8d4da70b3e5f9118018
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-08-22 00:21:19 +0700

    Fixed endpoint

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -127,17 +127,13 @@ async def create_meetings(meetings_info: CreateMeeting,
 
 @profile_router.get('/v1/meetings')
 async def get_meetings(user: User = Depends(current_active_user), session=Depends(get_async_session)) -> dict:
-    if user.role != "Mentor":
-        raise HTTPException(
-            status_code=400,
-            detail="You are not a Mentor"
-            )
     query = select(Meeting).where(Meeting.mentor_login == user.login)
     result = (await session.execute(query)).all()
     result_dict = dict()
     for index, item in enumerate(result):
         item = item[0]
-        result_dict[index + 1] = {
+        result_dict[item.id] = {
+            'id': index + 1,
             'user': item.user_login,
             'description': item.description,
             'start time': item.start_time
